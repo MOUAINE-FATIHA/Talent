e00f04155fba468f2157447b346b529fde11c042
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-02-02 07:44:12 +0100

    recherche des utilisateurs

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,8 +6,7 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function search(Request $request)
-    {
+    public function search(Request $request) {
         $q = $request->q;
         $users = User::where('name','like',"%$q%")
                     ->orWhere('specialite','like',"%$q%")
