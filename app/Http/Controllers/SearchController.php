74f9d59bfb8ba188dff33c11cc5c30a0817d40a8
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-02-03 15:18:52 +0100

    creation de controller search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function search(Request $request) {
-        $q = $request->q;
-        $users = User::where('name','like',"%$q%")
-                    ->orWhere('specialite','like',"%$q%")
-                    ->get();
-        return view('search.results', compact('users','q'));
+    public function index(Request $request){
+        $query = User::query();
+
+        if ($request->filled('name')){
+            $query->where('name','like', '%' . $request->name . '%');
+        }
+        if ($request->filled('specialite')){
+            $query->where('specialite', 'like', '%' . $request->specialite . '%');
+        }
+
+        $users = $query->whereNotNull('specialite')->get();
+        return view('search.results', compact('users'));
     }
 }
