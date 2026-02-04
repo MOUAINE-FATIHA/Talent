6019b1499bdcd985729e977642d2420ac5b142eb
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-02-04 12:20:47 +0100

    recuperation de détail d'un offre

--- app/Models/JobOffer.php
+++ app/Models/JobOffer.php
@@ -1,1 +1,14 @@
-JobOffer.php
+<?php
+class JobOffer extends Model{
+    protected $fillable = [
+        'titre',
+        'description',
+        'type_contrat',
+        'entreprise',
+        'image',
+        'recruiter_id'
+    ];
+    public function recruiter(){
+        return $this->belongsTo(Recruiter::class);
+    }
+}
